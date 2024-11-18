35114d92ba155c5004f4759c42550c0a780e960a
danya <danya001406@gmail.com>
2024-11-19 00:59:24 +0300

    Merge remote-tracking branch 'greenwoods/main'

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print("Hello World!")
+const
+url = 'https://work-kb8vsybsy-danyas-projects-f55a11c7.vercel.app/api/total-members';
--- src/components/pages/main.vue
+++ src/components/pages/main.vue
@@ -41,7 +41,7 @@ export default {
       window.Telegram.WebApp.BackButton.show();
     },
     async fetchTotalMembers() {
-      const url = 'https://work-kb8vsybsy-danyas-projects-f55a11c7.vercel.app/api/total-members';
+      const url = 'https://work-indol-one.vercel.app/api/total-members';
       console.log('Fetching total members from:', url); // Логируем URL
       try {
         const response = await axios.get(url);
