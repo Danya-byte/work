62836022316ced4689fa02f6c397d6c0b2e733dd
danya <danya001406@gmail.com>
2024-11-18 21:56:58 +0300

    Merge remote-tracking branch 'greenwoods/main'

--- src/components/pages/main.vue
+++ src/components/pages/main.vue
@@ -1,93 +1,14 @@
-<script>
-import axios from 'axios';
-import Background from '@/components/UI/bg.vue'
-import Headers from '@/components/UI/header.vue'
-import Footers from '@/components/UI/footer.vue'
-import Ref from './ref.vue'
-import Task from './task.vue'
-
-export default {
-  components: {
-    Background,
-    Headers,
-    Footers,
-    Ref,
-    Task
-  },
-  data() {
-    return {
-      show: 0,
-      totalMembers: ['0', '0', '0', '0'], // Массив для хранения символов числа, заполненный нулями по умолчанию
-    }
-  },
-  async mounted() {
-    window.Telegram.WebApp.MainButton.hide()
-    window.Telegram.WebApp.BackButton.onClick(() => {
-      this.show = 0
-      window.Telegram.WebApp.BackButton.hide()
-    })
-
-    // Вызываем метод для получения данных при монтировании компонента
-    await this.fetchTotalMembers();
-  },
-  methods: {
-    openRef() {
-      this.show = 1
-      window.Telegram.WebApp.BackButton.show()
-    },
-    openTask() {
-      this.show = 2
-      window.Telegram.WebApp.BackButton.show()
-    },
-    async fetchTotalMembers() {
-      // Замените URL на ваш реальный URL для получения данных
-      try {
-        const response = await axios.get('https://work-kb8vsybsy-danyas-projects-f55a11c7.vercel.app/api/total-members');
-        this.totalMembers = response.data.totalMembers; // Предполагаем, что ответ содержит массив цифр
-        console.log('Total Members Digits:', this.totalMembers.join(' ')); // Отладочный вывод
-      } catch (error) {
-        console.error('There was an error fetching the total members!', error);
-      }
-    }
-  }
-}
-</script>
-
-<template>
-  <Background />
-  <Headers :data="show" />
-  <main>
-    <nav class="cnt">
-      <div class="total">
-        <h1 style="font-family: Inter; font-size: 19px; color: #f0f0f0;">Total members</h1>
-      </div>
-      <div class="count">
-        <div>
-          <p>{{ totalMembers[0] }}</p>
-        </div>
-        <div class="nums">
-          <p v-for="(digit, index) in totalMembers.slice(1)" :key="index">{{ digit }}</p>
-        </div>
-      </div>
-    </nav>
-  </main>
-  <Footers @refOpen="openRef" @taskOpen="openTask" />
-  <Ref v-if="show === 1" />
-  <Task v-if="show === 2" />
-</template>
-
-<style scoped>
 main {
   position: absolute;
-    z-index: 10;
-    color: aliceblue;
-    bottom: 0;
-    top: 0;
-    left: 0;
-    right: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  z-index: 10;
+  color: aliceblue;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .cnt {
@@ -106,25 +27,25 @@ main {
 
 .count {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  min-width: 170px;
-  gap: 20px;
-}
-
-.nums {
-  display: flex;
   gap: 10px;
-  align-items: center;
-  justify-content: space-between;
 }
 
-p {
+.digit {
   background: #ffffff;
   padding: 12px;
   border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 40px;
+  min-height: 40px;
+}
+
+p {
   font-size: 20px;
   color: #000;
   font-weight: 700;
+  margin: 0;
 }
-</style>
